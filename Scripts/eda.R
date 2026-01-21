00bd7e5fdcf4928d285309c4c8a30efcc2c9628a
AlexEmert <alexemert@gmail.com>
2026-01-21 14:22:01 -0700

    attempt plot not working

--- Scripts/eda.R
+++ Scripts/eda.R
@@ -4,4 +4,8 @@ library(scryr)
 ltr_commanders <- scry_cards("is:commander set:ltr") |>
   unnest(prices)
   
-
+ltr_commmanders_df <- as.data.frame(ltr_commanders) |>
+  select(usd, cmc)
+ 
+  ggplot(ltr_commmanders_df, aes(x=cmc, y=usd)) +
+  geom_point()
