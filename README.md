5613469b006598c83d6eb6e9e99df61bb45bbc62
AlexEmert <alexemert@gmail.com>
2026-01-20 23:14:30 -0700

    fix read me, and restart looking at data

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CSCI2025-aemert-ind-proj
+# CSCI2025-aemert-ind-proj working
 This repo contains all of my work for the individual project in my freshman year winter term class, CSCI2025.
--- Scripts/Project-Code-Rough.R
+++ Scripts/Project-Code-Rough.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-ALLPringtingsCSVFiles <- "Individual-Project/Data/AllPrintingsCSVFiles.zip" 
-file_list <- unzip(ALLPringtingsCSVFiles, list = TRUE)
-print(file_list)
+#ALLPringtingsCSVFiles <- "CSCI22025-aemert-ind-projData/AllPrintingsCSVFiles.zip" 
+#file_list <- unzip(ALLPringtingsCSVFiles, list = TRUE)
+#print(file_list)
 
-file_to_extract <- "data1.csv" # Replace with the name of the file inside the zip
+#file_to_extract <- "data1.csv" # Replace with the name of the file inside the zip
