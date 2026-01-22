4a77687c2927b3348cfb44b63179206c756058f3
AlexEmert <alexemert@gmail.com>
2026-01-22 13:50:29 -0700

    scryr intro

--- Scripts/eda.R
+++ Scripts/eda.R
@@ -1,11 +1,6 @@
 library(tidyverse)
 library(scryr)
 
-ltr_commanders <- scry_cards("is:commander set:ltr") |>
-  unnest(prices)
-  
-ltr_commmanders_df <- as.data.frame(ltr_commanders) |>
-  select(usd, cmc)
- 
-  ggplot(ltr_commmanders_df, aes(x=cmc, y=usd)) +
-  geom_point()
+ltr_commanders_clean <- scry_cards("is:commander set:ltr")
+  unpack(prices) |>
+
