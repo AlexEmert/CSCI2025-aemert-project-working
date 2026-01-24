a494175e0f6d139b4ab5eccdcd8d7f3c17c0dc9b
AlexEmert <alexemert@gmail.com>
2026-01-24 16:36:48 -0700

    looking at data

--- Scripts/eda.R
+++ Scripts/eda.R
@@ -1,6 +1,22 @@
 library(tidyverse)
 library(scryr)
 
-ltr_commanders_clean <- scry_cards("is:commander set:ltr")
-  unpack(prices) |>
+card_price_data <- read_csv("Data/raw/AllPrintingsCSVFiles/cardPrices.csv")
+cards_data <- read_csv("Data/raw/AllPrintingsCSVFiles/cards.csv")
+set_data <- read_csv("Data/raw/AllPrintingsCSVFiles/sets.csv")
+set_booster_data <- read_csv("Data/raw/AllPrintingsCSVFiles/setBoosterContents.csv")
+
+glimpse(set_booster_data)
+
+llorwyn <- scry_cards("set:ECL") |>
+  unnest(prices)
+
+
+plot_data <- llorwyn |>
+  select(cmc, usd, edhrec_rank, cmc) |>
+  mutate(across(everything(), as.numeric)) |>
+  filter(!is.na(cmc))
+
+  ggplot(plot_data, aes(x=cmc, y = usd)) +
+  geom_point()
 
