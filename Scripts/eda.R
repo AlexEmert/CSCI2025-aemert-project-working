f84e667d1dcf435935f5ef78d707c2c21e342cfb
AlexEmert <alexemert@gmail.com>
2026-01-24 23:43:37 -0700

    mess around with scryr

--- Scripts/eda.R
+++ Scripts/eda.R
@@ -8,15 +8,30 @@ set_booster_data <- read_csv("Data/raw/AllPrintingsCSVFiles/setBoosterContents.c
 
 glimpse(set_booster_data)
 
+
+#scryr package exploration
 llorwyn <- scry_cards("set:ECL") |>
   unnest(prices)
-
 
 plot_data <- llorwyn |>
   select(cmc, usd, edhrec_rank, cmc) |>
   mutate(across(everything(), as.numeric)) |>
   filter(!is.na(cmc))
 
-  ggplot(plot_data, aes(x=cmc, y = usd)) +
+plot_data |>
+  filter(usd>=5)|>
+  ggplot(aes(x=cmc, y = usd)) +
   geom_point()
 
+#new approach
+scryfall_cards <- scry_cards("is:commander") |>
+  glimpse()
+
+scryfall_cards <- scryfall_cards |>
+  unnest(prices)
+
+scryfall_cards_sets <- scryfall_cards |>
+  select(set)
+
+  ggplot(scryfall_cards_sets, aes(x=set)) +
+  geom_bar()
