7c67bde39873289c51c01c5d87b12e215b1de737
AlexEmert <alexemert@gmail.com>
2026-01-27 10:14:37 -0700

    develop app

--- Scripts/app.R
+++ Scripts/app.R
@@ -1,11 +1,94 @@
 library(shiny)
 library(bslib)
+library(tidyverse)
+
+clean_data <- read_csv("Data/processed/cleaned_data.csv")
+
+mtg_data_unique <- clean_data |>
+  distinct(name, setCode, .keep_all = TRUE)
+
+
+simulate_booster <- function(mtg_data_unique, set_code_input) {
+
+  set_pool <- mtg_data_unique |>
+      filter(set_code_input == setCode)
+
+  commons <- set_pool  |>
+    filter(rarity == "common")
+  uncommons <- set_pool |>
+    filter(rarity =="uncommon")
+  rares <- set_pool |> 
+    filter(rarity == "rare")
+  mythics <- set_pool |> 
+    filter(rarity == "mythic")
+
+  rare_slot_type <- sample(c("rare", "mythic"), size = 1, prob = c(7/8, 1/8))
+
+  if (rare_slot_type == "rare") {
+      my_rare <- rares |>
+        sample_n(1)
+    } else {
+      my_rare <- mythics |>
+        sample_n(1)
+    }
+
+  my_commons <- commons |>
+    slice_sample(n=8)
+  my_uncommons <- uncommons |>
+    slice_sample(n=3)
+
+  pack <- bind_rows(my_commons, my_uncommons, my_rare)
+  
+  return(pack)
+}
+
 
 ui <- page_fluid(
+  titlePanel("Simulate some Booster Packs!"),
   
+  siderbarLayout(
+    sidebarPanel(
+      selectInput("setCodeChosen", label = "Select Set", 
+                  choices = ls("mtg_data_unique$setCode")),
+      actionButton("simulate_btn", "Simulate!", class = "btn-primary")
+    ),
+
+  mainPanel(
+    tableOutput("simTable"),
+    plotOutput("simPlot")
+    )
+  )
 )
 
 server <- function(input, output, session) {
+
+  simulated_data <- eventReactive(input$simulate_btn, {
+    
+    selected_val <- input$setCodeChosen
+    
+    first_pack <- simulate_booster(mtg_data_unique, selected_val)
+
+    sim_results <- replicate(1000, {
+      avg_pack <- simulate_booster(mtg_data_unique, selected_val)
+      sum(avg_pack$price, na.rm = TRUE)
+    }, simplify = TRUE)
+
+    return(sim_results)
+
+  })
+
+  ouput$simTable <- renderTable ({
+    head(simulated_data())
+  })
+
+  output$simPlot <- renderPlot ({
+    ggplot(sim_results, aes(x = total_value)) +
+    geom_histogram(fill = "steelblue", color = "white") +
+    geom_vline(xintercept = mean(sim_results), color = "red", linetype = "dashed") +
+    labs(title = "Distribution of Booster Pack Values",
+         x = "Total Pack Value ($)",
+         y = "Frequency")
+  })
 
 }
 
